25d41c93201202165a6336a865e098b334270e54
shuvoworld <shuvoworld@gmail.com>
2020-09-20 16:56:47 +0600

    #

--- employee.php
+++ employee.php
@@ -23,39 +23,33 @@ $pdocrud->fieldTypes("agency_id", "select"); //change type to select
 $pdocrud->addPlugin("select2");//to add plugin
 $pdocrud->fieldDataBinding("agency_id", $agency_query, "id", "name_BN", "sql");
 
-$pdocrud->fieldTypes("organization_type_id", "select"); //change type to select
-$pdocrud->fieldDataBinding("organization_type_id", "organization_types", "id", "name_BN", "db"); //load select data
+$pdocrud->fieldTypes("quota_id", "select"); //change type to select
+$pdocrud->fieldDataBinding("quota_id", "quotas", "id", "name", "db");
 
-$pdocrud->fieldTypes("organization_level_id", "select"); //change type to select
-$pdocrud->fieldDataBinding("organization_level_id", "organization_levels", "id", "name_BN", "db"); //load select data
+$pdocrud->fieldTypes("sex_id", "select"); //change type to select
+$pdocrud->fieldDataBinding("sex_id", "sex", "id", "name", "db");
+
+$pdocrud->fieldTypes("religion_id", "select"); //change type to select
+$pdocrud->fieldDataBinding("religion_id", "religions", "id", "name", "db");
+
+$pdocrud->fieldTypes("bloodgroup_id", "select"); //change type to select
+$pdocrud->fieldDataBinding("bloodgroup_id", "bloodgroups", "id", "name", "db");
 
 
 $pdocrud->fieldTypes("division_id", "select"); //change type to select
 $pdocrud->fieldDataBinding("division_id", "divisions", "id", "name_BN", "db"); //load select data
 
-$pdocrud->fieldTypes("district_id", "select"); //change type to select
-$pdocrud->fieldDataBinding("district_id", "districts", "id", "name_BN", "db"); //load select data
-
-$pdocrud->fieldTypes("upazila_id", "select"); //change type to select
-$pdocrud->fieldDataBinding("upazila_id", "upazilas", "id", "name_BN", "db"); //load select data
-
-$pdocrud->fieldDependent("district_id", "division_id", "division_id"); //now on change of country it will change state
-$pdocrud->fieldDependent("upazila_id", "district_id", "district_id"); //now on change of country it will change state
-
-$pdocrud->crudRemoveCol(array("name","organization_level_id","organization_type_id","contact_email", "cretaed_at", "is_active"));
+$pdocrud->crudRemoveCol(array("created_at", "is_active", "code"));
 
 //$pdocrud->crudTableCol(array("ID","Name","Directorate","Division","District", "Upazila", "Contact"));//optional
 //$pdocrud->relatedData('agency_id','agencies','id','name_BN');
-$pdocrud->relatedData('division_id','divisions','id','name_BN');
-$pdocrud->relatedData('district_id','districts','id','name_BN');
-$pdocrud->relatedData('upazila_id','upazilas','id','name_BN');
+// $pdocrud->relatedData('division_id','divisions','id','name_BN');
+// $pdocrud->relatedData('district_id','districts','id','name_BN');
+// $pdocrud->relatedData('upazila_id','upazilas','id','name_BN');
 
 $pdocrud->fieldRenameLable("agency_id", "Agency");//Rename label
-$pdocrud->fieldRenameLable("division_id", "Division");//Rename label
-$pdocrud->fieldRenameLable("district_id", "District");//Rename label
-$pdocrud->fieldRenameLable("upazila_id", "Upazila");//Rename label
-$pdocrud->fieldRenameLable("organization_type_id", "Type");//Rename label
-$pdocrud->fieldRenameLable("organization_level_id", "Level");//Rename label
+$pdocrud->fieldRenameLable("name_BN", "Name (Bangla)");//Rename label
+$pdocrud->fieldRenameLable("name_BN", "Name (English)");//Rename label
 
 $pdocrud->fieldDataAttr("created_at", array("disabled"=>"disabled"));
 $pdocrud->fieldHideLable("created_by");
@@ -71,9 +65,26 @@ $pdocrud->formFieldValue("is_active", 1);
 $pdocrud->fieldHideLable("is_active");
 $pdocrud->fieldDataAttr("is_active", array("style"=>"display:none"));
 
-$pdocrud->fieldGroups("Naming",array("name","name_BN"));
-$pdocrud->fieldGroups("Organization Attribute",array("agency_id","organization_type_id","organization_level_id"));
-$pdocrud->fieldGroups("Geo Location",array("division_id","district_id", "upazila_id"));
+$pdocrud->fieldHideLable("quota_name");
+$pdocrud->fieldDataAttr("quota_name", array("style"=>"display:none"));
+$pdocrud->fieldDataAttr("quota_name", array("disabled"=>"disabled"));
+
+$pdocrud->fieldHideLable("sex_name");
+$pdocrud->fieldDataAttr("sex_name", array("style"=>"display:none"));
+$pdocrud->fieldDataAttr("sex_name", array("disabled"=>"disabled"));
+
+$pdocrud->fieldHideLable("religious_name");
+$pdocrud->fieldDataAttr("religious_name", array("style"=>"display:none"));
+$pdocrud->fieldDataAttr("religious_name", array("disabled"=>"disabled"));
+
+$pdocrud->fieldHideLable("bloodgroup_name");
+$pdocrud->fieldDataAttr("bloodgroup_name", array("style"=>"display:none"));
+$pdocrud->fieldDataAttr("bloodgroup_name", array("disabled"=>"disabled"));
+
+$pdocrud->fieldGroups("Naming",array("agency_id", "name_BN","name_EN"));
+$pdocrud->fieldGroups("family_info",array("father_name","mother_name"));
+$pdocrud->fieldGroups("id_contact",array("national_id","mobile_no", "email"));
+$pdocrud->fieldGroups("attribute",array("quota_id","sex_id", "religion_id", "bloodgroup_id"));
 
 
 
@@ -81,6 +92,6 @@ $pdocrud->addCallback("before_update", "beforeUpdateCallBack");
 
 
 
-echo $pdocrud->dbTable("organizations")->render();
+echo $pdocrud->dbTable("employees")->render();
 echo $pdocrud->loadPluginJsCode("select2","select");//to add plugin call on select elements
 ?>
